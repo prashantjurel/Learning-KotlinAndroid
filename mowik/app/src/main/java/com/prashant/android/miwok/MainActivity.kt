33ffeee799b06801aac3b1acdea9a81262b19648
prashantjurel <prashantjurel@outlook.com>
2021-06-10 00:20:25 +0530

    navigate activites on btnClick

--- mowik/app/src/main/java/com/prashant/android/miwok/MainActivity.kt
+++ mowik/app/src/main/java/com/prashant/android/miwok/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.prashant.android.miwok
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.prashant.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNumbersList(view: View) {
+        val intent = Intent(this,ColorsActivity::class.java)
+        startActivity(intent)
     }
 }
