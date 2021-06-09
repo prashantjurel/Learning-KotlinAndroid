93e650b126f438141007ee02c4976b07eace1a1b
prashantjurel <prashantjurel@outlook.com>
2021-06-09 23:23:36 +0530

    Added new activities

---
+++ mowik/app/src/main/java/com/prashant/android/miwok/ColorsActivity.kt
@@ -0,0 +1,12 @@
+package com.prashant.android.miwok
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.prashant.android.R
+
+class ColorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_colors)
+    }
+}
---
+++ mowik/app/src/main/java/com/prashant/android/miwok/NumbersActivity.kt
@@ -0,0 +1,12 @@
+package com.prashant.android.miwok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prashant.android.R
+
+class NumbersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numbers)
+    }
+}
