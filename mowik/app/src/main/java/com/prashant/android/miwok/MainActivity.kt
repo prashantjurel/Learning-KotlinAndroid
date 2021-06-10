7f59338171958fbdb656e0913987b10a7455294a
prashantjurel <prashantjurel@outlook.com>
2021-06-10 12:31:05 +0530

    added explicit intent on btnClick

--- mowik/app/src/main/java/com/prashant/android/miwok/MainActivity.kt
+++ mowik/app/src/main/java/com/prashant/android/miwok/MainActivity.kt
@@ -2,18 +2,42 @@ package com.prashant.android.miwok
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.prashant.android.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var numbersBtn : TextView
+    private lateinit var familyBtn : TextView
+    private lateinit var colorsBtn : TextView
+    private lateinit var phrasesBtn : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numbersBtn = findViewById(R.id.numbers)
+        familyBtn = findViewById(R.id.family)
+        colorsBtn = findViewById(R.id.colors)
+        phrasesBtn = findViewById(R.id.phrases)
+
+        numbersBtn.setOnClickListener{
+            val intent = Intent(this,NumbersActivity::class.java)
+            startActivity(intent)
+        }
+        familyBtn.setOnClickListener{
+            val intent = Intent(this,FamilyActivity::class.java)
+            startActivity(intent)
+        }
+        colorsBtn.setOnClickListener{
+            val intent = Intent(this,ColorsActivity::class.java)
+            startActivity(intent)
+        }
+        phrasesBtn.setOnClickListener{
+            val intent = Intent(this,PhrasesActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    fun openNumbersList(view: View) {
-        val intent = Intent(this,ColorsActivity::class.java)
-        startActivity(intent)
-    }
 }
