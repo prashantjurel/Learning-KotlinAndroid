d35f807001d64814d07f8e6b88ca13825e55d32f
prashantjurel <prashantjurel@outlook.com>
2021-06-12 02:05:45 +0530

    Modify adapter for taking both phrases/Others

--- mowik/app/src/main/java/com/prashant/android/miwok/Word.kt
+++ mowik/app/src/main/java/com/prashant/android/miwok/Word.kt
@@ -1,8 +1,23 @@
 package com.prashant.android.miwok
 
-class Word (miwokParam:String, defaultParam:String){
+import com.prashant.android.miwok.Word.Constants.NO_IMAGE_PROVIDED
+
+class Word (miwokParam:String,defaultParam:String,resourceId:Int){
+
+    object Constants{
+        const val NO_IMAGE_PROVIDED = -1
+    }
+
+    constructor(miwokParam:String, defaultParam:String) : this(
+        miwokParam,defaultParam, NO_IMAGE_PROVIDED)
+
     var miwokText:String = miwokParam
-    private set
+        private set
     var defaultText:String = defaultParam
-    private set
+        private set
+    var resource:Int = resourceId
+        private set
+
+    fun hasImage() : Boolean { return resource!= NO_IMAGE_PROVIDED }
+
 }
--- mowik/app/src/main/java/com/prashant/android/miwok/WordAdapter.kt
+++ mowik/app/src/main/java/com/prashant/android/miwok/WordAdapter.kt
@@ -3,8 +3,11 @@ package com.prashant.android.miwok
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.prashant.android.R
 
@@ -12,12 +15,20 @@ class WordAdapter(context: Activity, numbersList: ArrayList<Word>) : ArrayAdapte
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val listItemView = convertView ?: run {
-            var inflater = LayoutInflater.from(context)
+            val inflater = LayoutInflater.from(context)
             inflater.inflate(R.layout.list_item, parent, false)!!
         }
-            var currentWord: Word = getItem(position)!!
+            val currentWord: Word = getItem(position)!!
             listItemView.findViewById<TextView?>(R.id.mowik_text_view)?.text = currentWord.miwokText
             listItemView.findViewById<TextView?>(R.id.default_text_view)?.text = currentWord.defaultText
+            if(currentWord.hasImage()) {
+                listItemView.findViewById<ImageView?>(R.id.mowik_image_view)
+                    ?.setImageResource(currentWord.resource)
+                listItemView.findViewById<ImageView?>(R.id.mowik_image_view)
+                    ?.visibility = VISIBLE
+            }
+            else
+                listItemView.findViewById<ImageView?>(R.id.mowik_image_view)?.visibility = GONE
 
             return listItemView
         }
