0d3656a14c5af8f5884f6f4c1bf672ab4f602e26
prashantjurel <prashantjurel@outlook.com>
2021-06-15 01:00:40 +0530

    added vector drawable support

--- diceRoller/app/src/main/java/com/prashant/diceroller/MainActivity.kt
+++ diceRoller/app/src/main/java/com/prashant/diceroller/MainActivity.kt
@@ -7,15 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
-        val diceImage : ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (Random.nextInt(6)+1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
